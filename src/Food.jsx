dca27e0e4798a842f82b9f79564dec525883e6a2
Sasmitha_Sudusinghe <sasmithasudusinghe@gmail.com>
2026-09-17 22:38:14 +0530

    Add files via upload

---
+++ src/Food.jsx
@@ -0,0 +1,13 @@
+function Food(){
+
+    const food1="Orange";
+    const food2="Apple";
+
+    return(
+        <div>
+            <p>{food1}</p>
+            <p>{food2}</p>
+        </div>
+    );
+}
+export default Food
---
+++ src/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer(){
+    return(
+        <footer>
+            <p>&copy; {new Date().getFullYear()} My React App. All rights reserved.</p>
+        </footer>
+    );
+}
+export default Footer
